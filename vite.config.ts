e9d35dc09e5aab6ce5bd89c52091b8053f453db7
Ahmad920 <ahaashoor@hotmail.com>
2025-04-20 06:43:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/prayer-times/', // سيتم استبدال هذا باسم مشروعك على GitHub
+  base: '/islamic-prayer-and-third-part-of-night/', // سيتم استبدال هذا باسم مشروعك على GitHub
   server: {
     host: "::",
     port: 8080,
